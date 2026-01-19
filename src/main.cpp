8605e75b21c6a565afb1d3b750ad296773f6f803
olivier-fiset <olivierfiset@outlook.com>
2026-01-19 00:18:24 -0500

    vitesse selon odds

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include <Arduino.h>
 #include "MCPWM.h"
+#include "IO.h"
 
 // put function declarations here:
-#define CHEVAL_1 0
-#define CHEVAL_2 1
-#define CHEVAL_3 2
-#define CHEVAL_4 3
+
+const int NB_CHEVAUX = 4;
+const int BUFFER_SIZE = 50;
+const uint8_t odds[NB_CHEVAUX] = {2, 3, 5, 10};
+uint8_t vitesseBuffer[NB_CHEVAUX][BUFFER_SIZE];
 
 #define INTERVALLE_PAR_VITESSE_TICKS pdMS_TO_TICKS(500)
 bool courseFinie = false;
-uint8_t bufferVitesse1[10] = {0};
-uint8_t bufferVitesse2[10] = {0};
-uint8_t bufferVitesse3[10] = {0};
-uint8_t bufferVitesse4[10] = {0};
-void remplirBufferVitesses(void);
+
+void remplirBuffers();
+void courseFinieCallback();
+void setNewSpeeds(uint8_t index);
 
 void setup()
 {
@@ -22,6 +23,10 @@ void setup()
   MCPWM_init(MOTOR_2);
   MCPWM_init(MOTOR_3);
   MCPWM_init(MOTOR_4);
+
+  IO_initAllInputs(MCP23017_GPIOA_REG);
+  IO_initAllInputs(MCP23017_GPIOB_REG);
+  // attachInterrupt(digitalPinToInterrupt(1), courseFinieCallback, RISING);
 }
 
 void loop()
@@ -40,13 +45,17 @@ void loop()
   uint32_t currentTick;
   uint32_t lastSpeedChangeTick = 0;
   uint8_t currentIndex = 0;
+  remplirBuffers();
+  setNewSpeeds(currentIndex);
 
   while(!courseFinie){
     currentTick = xTaskGetTickCount();
     if(currentTick - lastSpeedChangeTick >= INTERVALLE_PAR_VITESSE_TICKS){
       currentIndex++;
     }
-    // courseFinie = IO_read();
+  }
+  while(courseFinie){
+    // read IO for button to start new race
   }
 
 
@@ -54,13 +63,57 @@ void loop()
 }
 
 // put function definitions here:
-void remplirBufferVitesses(void)
-{
 
-  for(uint8_t i = 0; i < 10; i++){
-    bufferVitesse1[i];
-    bufferVitesse2[i];
-    bufferVitesse3[i];
-    bufferVitesse4[i];
-  }
+void setNewSpeeds(uint8_t index){
+  MCPWM_setSpeed(MOTOR_1, vitesseBuffer[0][index]);
+  MCPWM_setDirection(MOTOR_1, FORWARD);
+
+  MCPWM_setSpeed(MOTOR_2, vitesseBuffer[1][index]);
+  MCPWM_setDirection(MOTOR_2, FORWARD);
+
+  MCPWM_setSpeed(MOTOR_3, vitesseBuffer[2][index]);
+  MCPWM_setDirection(MOTOR_3, FORWARD);
+
+  MCPWM_setSpeed(MOTOR_4, vitesseBuffer[3][index]);
+  MCPWM_setDirection(MOTOR_4, FORWARD);
+}
+
+
+void courseFinieCallback(){
+  courseFinie = true;
+
+  MCPWM_setSpeed(MOTOR_1, 0);
+  MCPWM_setDirection(MOTOR_1, STOP);
+
+  MCPWM_setSpeed(MOTOR_2, 0);
+  MCPWM_setDirection(MOTOR_2, STOP);
+
+  MCPWM_setSpeed(MOTOR_3, 0);
+  MCPWM_setDirection(MOTOR_3, STOP);
+
+  MCPWM_setSpeed(MOTOR_4, 0);
+  MCPWM_setDirection(MOTOR_4, STOP);
+}
+
+void remplirBuffers() {
+    float performance[NB_CHEVAUX];
+    float somme = 0;
+
+    // Calculer la performance normalisée
+    for (int i = 0; i < NB_CHEVAUX; i++) {
+        performance[i] = 1.0 / odds[i]; // inverse des odds
+        somme += performance[i];
+    }
+    for (int i = 0; i < NB_CHEVAUX; i++) {
+        performance[i] /= somme; // normalisation
+    }
+
+    // Remplir les buffers
+    for (int i = 0; i < NB_CHEVAUX; i++) {
+        for (int j = 0; j < BUFFER_SIZE; j++) {
+            float r = random(0, 1000) / 1000.0; // 0..1 aléatoire
+            float biais = pow(r, 1.0 / (performance[i] * 10 + 0.1));
+            vitesseBuffer[i][j] = (uint8_t)(biais * 100.0); // convertir en 0..100
+        }
+    }
 }
